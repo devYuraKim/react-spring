b11d52935d2bb1aa4dd24eab81af8d2d421d3c99
devYuraKim <dev.yurakim@gmail.com>
2024-08-19 20:02:42 +0900

    #issue1 'required' attribute ignored
    event.preventDefault() needed

--- 1ReactCrashCourse/src/App.js
+++ 1ReactCrashCourse/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "./Components/TodoRowItem";
 import TodoTable from "./Components/TodoTable";
+import NewTodoForm from "./Components/NewTodoForm";
 
 const todoList = [
   { rowNumber: 1, rowDescription: "Feed the dog", rowAssigned: "Eric" },
@@ -12,14 +13,15 @@ const todoList = [
 
 function App() {
   const [todos, setTodos] = useState(todoList);
+  const [showForm, setShowForm] = useState(false);
 
-  function addTodo() {
+  function addTodo(newTodo) {
     setTodos((todos) => [
       ...todos,
       {
         rowNumber: todos.length + 1,
-        rowDescription: "new todo item",
-        rowAssigned: "user",
+        rowDescription: newTodo.description,
+        rowAssigned: newTodo.assigned,
       },
     ]);
   }
@@ -30,9 +32,17 @@ function App() {
         <div className="card-header">Your Todos</div>
         <div className="card-body">
           <TodoTable todos={todos}></TodoTable>
-          <button className="btn btn-primary" onClick={addTodo}>
-            Add a new todo
-          </button>
+          {!showForm && (
+            <button
+              className="btn btn-primary mt-3"
+              onClick={() => setShowForm((prev) => !prev)}
+            >
+              Add a new todo
+            </button>
+          )}
+          {showForm && (
+            <NewTodoForm addTodo={addTodo} setShowForm={setShowForm} />
+          )}
         </div>
       </div>
     </div>
