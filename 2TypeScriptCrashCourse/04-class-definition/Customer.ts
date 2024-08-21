647d58e74693e131313d01e723e2aaa622c94f13
devYuraKim <dev.yurakim@gmail.com>
2024-08-21 23:27:00 +0900

    create classes w/ typescript

---
+++ 2TypeScriptCrashCourse/04-class-definition/Customer.ts
@@ -0,0 +1,11 @@
+class Customer {
+  firstName: string;
+  lastName: string;
+}
+
+let myCustomer = new Customer();
+myCustomer.firstName = "Marine";
+myCustomer.lastName = "LePenne";
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
